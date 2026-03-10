9558595a18643e7d2906f9c1c16bfc20b80fa376
Alex S <Alexander2.Silvester@live.uwe.ac.uk>
2026-03-10 12:07:36 +0000

    Added collection area to scene

--- Assets/Scripts/DropOffManager.cs
+++ Assets/Scripts/DropOffManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,10 +7,12 @@ public class DropOffManager : MonoBehaviour
     [SerializeField] int requiredBoxes = 1;
     int collectedBoxes = 0;
 
+    [SerializeField] List<string> pickupBoxTypes = new List<string>();
+
     [SerializeField] UnityEvent completionEvent;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag != "Pickup" || collectedBoxes == requiredBoxes) return;
+        if (!pickupBoxTypes.Contains(other.gameObject.tag) || collectedBoxes == requiredBoxes) return;
 
         collectedBoxes++;
         Destroy(other.gameObject);
